06575c7ff4c29ce6dfd5d89bfbdabefca93c0e03
ntrupin <natrupin@gmail.com>
2022-01-05 19:39:30 -0500

    Fix teams

--- static/script.js
+++ static/script.js
@@ -2,9 +2,10 @@ const members = document.querySelector("#members");
 fetch("./static/team.json")
     .then(response => response.json())
     .then(data => data.forEach(e => {
+        img = e.hasOwnProperty("image") ? `<img src="${e["image"]}" />` : ``
         members.innerHTML += `
         <li>
-            <img src="${e["image"]}" />
+            ${img}
             <p>
                 <b>${e["name"]}</b>
                 <br />
